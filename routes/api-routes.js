91d8c733c2ee274f3dbe2691721edd4e9f6d0bd5
Jessica <jessicamenius@gmail.com>
2020-06-28 13:50:58 -0700

    adding html to index adn notes

---
+++ routes/api-routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+router.get("/api", (req, res) => {
+  res.json({ msg: "success" });
+});
+
+router.get("/api/all", (req, res) => {
+  res.json({ msg: "success" });
+  //   fs.readFile("data.json", "utf8", (err, data) => {
+  //     if (err) throw err;
+  //     data = JSON.parse(data);
+  //     res.send(data);
+  //   });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./client/"));
+app.use(express.static("./client"));
 
 const clientRoutes = require("./routes/client-routes");
 app.use(clientRoutes);
