8392d491a45a22340484950a2094edd7d2b2e12b
Jessica <jessicamenius@gmail.com>
2020-06-29 15:37:53 -0700

    working on client-routes.js

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -10,4 +10,8 @@ router.get("/add", (req, res) => {
   res.sendFile(path.join(___dirname, "../client/add.html"));
 });
 
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/notes.html"));
+});
+
 module.export = router;
